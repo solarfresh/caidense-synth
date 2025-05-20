93a2e5d95c04b7e6fd0e904f85fe8091c53a53b8
Scott Huang <shangyuhuang@gmail.com>
2025-05-21 00:20:54 +0800

    docs(api): Add Swagger documentation to ExecutionController
    Adds Swagger annotations to the `sendMessage` endpoint in
    `ExecutionController`.
    
    This includes:
    - `@ApiOperation` for a summary description.
    - `@ApiResponse` for common success and error statuses.
    - `@ApiBody` to describe the expected request body type.
    
    These additions improve the API documentation, making it easier
    for developers to understand and interact with the `/execution/send` endpoint.

--- api/src/modules/execution/execution.controller.ts
+++ api/src/modules/execution/execution.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ExecutionProducer } from './execution.producer';
+
 
 @Controller('execution')
 export class ExecutionController {
   constructor(private readonly executionProducer: ExecutionProducer) {}
 
   @Post('send')
+  @ApiOperation({ summary: 'Send a message to RabbitMQ' })
+  @ApiResponse({ status: 200, description: 'Message sent successfully.' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiBody({
+    description: 'Message content to be sent to RabbitMQ',
+    type: String,
+  })
   async sendMessage(@Body('message') message: string) {
     if (!message) {
       return { status: 'error', message: 'Message content is required.' };
